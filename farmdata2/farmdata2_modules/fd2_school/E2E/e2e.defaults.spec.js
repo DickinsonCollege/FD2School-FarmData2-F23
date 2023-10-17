cc627f1a4f2e26014e8bb1d7469a13dcf09ef37e
aidanbennettjones <bennetai@dickinson.edu>
2023-10-17 17:43:31 -0400

    Used wierd/different commands because testing would not load for awhile

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
@@ -8,4 +8,12 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=page-header]")
             .should("have.text","Harvest Report")
     })
+    it("Select Start and End Inputs"), () => {
+        const startDate = "05/05/2020".split('/').reverse().join('-');
+        const endDate = "05/15/2020".split('/').reverse().join('-');
+        cy.get('[data-cy=start-date-input]')
+            .should('have.value', '05/05/2020');
+        cy.get('[data-cy=end-date-input]')
+           .should('have.value', '05/15/2020');
+    }
 })
