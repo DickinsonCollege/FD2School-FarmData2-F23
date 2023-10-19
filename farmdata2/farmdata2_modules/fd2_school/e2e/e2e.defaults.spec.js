9e415947db8f7bcc8a0dbbbcbdd8c311dd4ce676
VPhung1901 <phungv@dickinson.edu>
2023-10-19 19:16:21 -0400

    chck crop-dropdown in e2e.defaults.spec.js

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -16,4 +16,22 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=end-date]")
         .should("have.value","2020-05-15")
     }) 
+
+    it("Check the crop dropdown", () => { 
+        // let the page loading the crop-dropdown
+        cy.get("[data-cy=crop-dropdown]")
+        .wait(10000)
+        // check first crop name
+        cy.get("[data-cy=crop-dropdown]").children().eq(0)
+        .should("have.value","ARUGULA")
+        // check fifth crop name
+        cy.get("[data-cy=crop-dropdown]").children().eq(4)
+        .should("have.value","BEAN-FAVA")
+        // check last crop name
+        cy.get("[data-cy=crop-dropdown]").children().eq(-1)
+        .should("have.value","ZUCCHINI")
+        // check total number of crops
+        cy.get("[data-cy=crop-dropdown]").children()
+        .should("have.length",111)
+    }) 
 }) 
