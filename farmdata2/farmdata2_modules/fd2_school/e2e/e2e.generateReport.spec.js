4385c7fd2a9807da013cfe94ed171ba37ad927ea
allisons9988 <smithaj@dickinson.edu>
2023-10-19 15:42:57 -0400

    Added test for farm name, username, and language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
@@ -9,4 +9,11 @@ describe("Test the harvest report values after report is generated", () => {
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=title]").should("be.visible")
     })
+    it("Check names", () => {
+        cy.get("[data-cy=generate-button]")
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text","Farm:Sample Farm")
+        cy.get("[data-cy=username]").should("contain.text","manager1")
+        cy.get("[data-cy=language]").should("have.text","English")
+    })
 })
