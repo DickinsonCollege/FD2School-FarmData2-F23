d0981824f30102d4eb821971b348f5efc8646424
minhphung152 <phungm@dickinson.edu>
2023-10-20 10:10:41 -0400

    Create test for crop dropdown

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -12,4 +12,11 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=start-date]").should("have.value", "2020-05-05")
         cy.get("[data-cy=end-date]").should("have.value", "2020-05-15")
     })
+
+    it("Check the crop dropdown", () => {
+        cy.get("[data-cy=crop]").children().eq(0).should("have.text", "All")
+        cy.get("[data-cy=crop]").children().eq(4).should("have.text", "BEAN-DRY")
+        cy.get("[data-cy=crop]").children().eq(111).should("have.text", "ZUCCHINI")
+        cy.get("[data-cy=crop]").children().should("have.length", "112")
+    })
 })
