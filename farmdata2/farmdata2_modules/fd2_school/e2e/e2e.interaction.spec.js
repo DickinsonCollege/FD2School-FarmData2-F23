143d62c1d1622ba5bab780dbe6a83eca0dee2100
minhphung152 <phungm@dickinson.edu>
2023-10-20 12:46:23 -0400

    Create test for farm nane, username and language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interaction.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interaction.spec.js
@@ -9,4 +9,11 @@ describe("Test the behavior of the Generate Report button", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-title]").should("be.visible")
     })
+
+    it("Check the information about the farm", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text", "Farm:Sample Farm")
+        cy.get("[data-cy=username]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "English")
+    })
 })
