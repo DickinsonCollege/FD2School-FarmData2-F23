16b0358d6318a1b2ba6edfe56f4e206f8965515a
balemae <balemae@dickinson.edu>
2023-10-20 02:31:26 -0400

    Added tests for farm name, username and language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -13,7 +13,23 @@ describe("Test the harvest report generated values", () => {
         cy.get("[data-cy=report-title]")
             .should("be.visible") 
           
+    })
+
+    it("Check farm name", () => {
+        cy.get("[data-cy=farm-name]").not.exist
+        cy.get("[data-cy=user-name]").not.exist
+        cy.get("[data-cy=language]").not.exist
+
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]")
+        .should("have.text", "Farm:Sample Farm")
+        cy.get("[data-cy=user-name]")
+        .should("contain.text", "manager1")
+        cy.get("[data-cy=language]")
+        .should("have.text", "English")
 
 
     })
+
+
 })
