3698fe03630016a98d4011b128f3f6ff64e29ae8
nghtlinh <nghtlinh25@gmail.com>
2023-10-20 01:45:34 -0400

    Tested crop dropdown options in e2e

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -12,4 +12,18 @@ describe("Test the harvest report default values", () => {
     cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
     cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
   })
+
+  it("Check the crop and area options", () => {
+    cy.get("[data-cy=crop-dropdown]").children().eq(0)
+    .should("have.value","ARUGULA")
+    
+    cy.get("[data-cy=crop-dropdown]").children().eq(4)
+    .should("have.value","BEAN-FAVA")
+    
+    cy.get("[data-cy=crop-dropdown]").children().eq(-1)
+    .should("have.value","ZUCCHINI")
+    
+    cy.get("[data-cy=crop-dropdown]").children()
+    .should("have.length",111)
+  })
 })
