5b738aa5a0e107566fb00dd5ead3c82ad1961bdd
charliemax5 <maxwellc@dickinson.edu>
2023-10-19 23:31:07 -0400

    Added Cypress test to e2e.genReport.spec.js to check values for Farm, Username, and Language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.genReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.genReport.spec.js
@@ -10,4 +10,14 @@ describe("Test the results of the Generate Report Button", () => {
         cy.get("[data-cy=title]")
             .should("be.visible")
     })
+
+    it("Check correct Farm, Username, and Language", () => {
+        cy.get("[data-cy=Generate-Report-Button]").click()
+        cy.get("[data-cy=farm]")
+            .should("have.text","Farm: Sample Farm")
+        cy.get("[data-cy=username]")
+            .should("contain.text","manager1")
+        cy.get("[data-cy=language]")
+            .should("have.text","English")
+    })
 })
