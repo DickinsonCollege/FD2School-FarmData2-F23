9f88f71e4e0bbaabb90c6f72a48700c326cf6fa2
aidanbennettjones <bennetai@dickinson.edu>
2023-10-19 20:55:04 -0400

    Some of my tests still do not work.

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.generateReport.spec.js
@@ -10,5 +10,11 @@ describe("Test Report Generation", () => {
       cy.get("[data-cy=generate-button]").click()
       cy.get("[data-cy=report-header]").should.exist
     });
+    it("Checks if the farm name is correctly set after generating the report & names", () => {
+        cy.get('[data-cy="generate-button"]').click();
+        cy.get('[data-cy="farm-name"]').should("contain.text", "Sample Farm"); 
+        cy.get('[data-cy="user-name"]').should("contain.text", "manager1");
+        cy.get('[data-cy="user-lang"]').should("have.text", "en");
+      });
   }); 
   
