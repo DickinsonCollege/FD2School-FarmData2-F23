fc43f28389685e3cf185927e2b77a02a2807cdac
nghtlinh <nghtlinh25@gmail.com>
2023-10-21 02:00:34 -0400

    Tested names of farm, user, language by cypress

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
@@ -9,4 +9,11 @@ describe("Test the output after generate report", () => {
     cy.get("[data-cy=generate-report-button]").click()
     cy.get("[data-cy=report-title]").should("be.visible")
   }) 
+
+  it("Check name of farm, user, language", () => {
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=farm-name]").should("have.text","Farm:Sample Farm")
+    cy.get("[data-cy=user-name]").should("contain.text","manager1")
+    cy.get("[data-cy=lang-name]").should("have.text","English")
+  })
 })
