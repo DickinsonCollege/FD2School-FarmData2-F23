17e6d752f30df0fcda795f17bb6c378eacf6b74b
hutterfly <huttere@dickinsno.edu>
2023-10-21 16:05:28 -0400

    added test for report details

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report-generation.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report-generation.spec.js
@@ -9,4 +9,11 @@ describe("Test the harvest report generation", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-title]").should("be.visible")
     })
+
+    it("Check the report details", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text", "Sample Farm")
+        cy.get("[data-cy=user-name]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "English")
+    })
 })
