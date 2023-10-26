c24e515122d87f32bbcfc4b4bb3c11a071cd2b56
bkollmar <kollmarb@dickinson.edu>
2023-10-26 17:54:06 -0400

    added blank cypress test file for fd2 table

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,12 @@
+describe("Test contents of report table", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+
+    it("", () => {
+
+    })
+
+    
+})
