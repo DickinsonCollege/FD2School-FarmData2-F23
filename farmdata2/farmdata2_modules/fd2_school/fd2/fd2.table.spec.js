0a88f4879ee00ffcfd38062758dd6837263a8276
balemae <balemae@dickinson.edu>
2023-10-27 12:42:10 -0400

    Added tests to check headers of the Harvest Report table

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -1,9 +1,15 @@
-describe("Test the harvest report default values", () => {
+describe("Test the harvest report table", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/fd2")
     })
       
-    it("", () => {
+    it("Check the table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text", "Date")
+        cy.get("[data-cy=h1]").should("have.text", "Crop")
+        cy.get("[data-cy=h2]").should("have.text", "Area")
+        cy.get("[data-cy=h3]").should("have.text", "Yield")
+        cy.get("[data-cy=h4]").should("have.text", "Units")
     })
 })
