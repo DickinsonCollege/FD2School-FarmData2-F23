f323909abbb18ed8d8498ac378b9376197cd3511
charliemax5 <maxwellc@dickinson.edu>
2023-10-26 22:35:04 -0400

    Created fd2.table.spec.js Cypress test file

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Test if the CustomTableComponent works", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+
+    it("", () => {
+
+    })
+})
