d61baf29d0e89b349df972f269f333b2b6e2b029
charliemax5 <maxwellc@dickinson.edu>
2023-10-26 22:41:41 -0400

    Added Cypress test to fd2.table.spec.js to check table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,17 @@ describe("Test if the CustomTableComponent works", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
 
-    it("", () => {
-
+    it("Check table headers", () => {
+        cy.get("[data-cy=Generate-Report-Button]").click()
+        cy.get("[data-cy=h0]")
+            .should("have.text","Date")
+        cy.get("[data-cy=h1]")
+            .should("have.text","Area")
+        cy.get("[data-cy=h2]")
+            .should("have.text","Crop")
+        cy.get("[data-cy=h3]")
+            .should("have.text","Units")
+        cy.get("[data-cy=h4]")
+            .should("have.text","Yield")
     })
 })
