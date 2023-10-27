0623d3b31164f3fb84140ccf6e69633f43a4ffe4
nghtlinh <nghtlinh25@gmail.com>
2023-10-27 10:33:52 -0400

    Make the test visit fd2 page

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generateReport.spec.js
@@ -1,7 +1,7 @@
 describe("Test the output after generate report", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
   
     it("Test the generate report button", () => { 
