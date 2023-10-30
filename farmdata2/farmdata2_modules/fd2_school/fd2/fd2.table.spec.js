a4b954048561d365e6878c5f9393de2b1cb40b47
hutterfly <huttere@dickinsno.edu>
2023-10-29 22:58:21 -0400

    Testing with the CustomTableComponent

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Testing with the CustomTableComponent", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+      
+    it("do something", () => {
+
+    })
+})
