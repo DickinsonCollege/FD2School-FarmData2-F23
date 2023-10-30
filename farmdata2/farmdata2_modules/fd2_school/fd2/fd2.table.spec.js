37e7c2c1c72f0746c2167ce701c5794520f37fc6
hutterfly <huttere@dickinsno.edu>
2023-10-29 23:14:24 -0400

    finished test that checks the headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -5,6 +5,12 @@ describe("Testing with the CustomTableComponent", () => {
     })
       
     it("do something", () => {
-
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Row")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
     })
 })
