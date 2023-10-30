0024029a72172ac239fc953522864c31cb14df6e
balemae <balemae@dickinson.edu>
2023-10-30 17:20:34 -0400

    added test to check time unit dropdown options

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.labor.defaults.spec.js
@@ -0,0 +1,16 @@
+describe('Test the seeding input page', () => {
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    }) 
+
+    it("Testing time units dropdown", () => {
+        //Checks the time units dropdown has the options minutes and hours
+        cy.get("[data-cy=time-unit] > [data-cy=dropdown-input] > [data-cy=option0]")
+           .should("have.text", "minutes")
+        cy.get("[data-cy=time-unit] > [data-cy=dropdown-input] > [data-cy=option1]")
+            .should("have.text", "hours")
+
+    })
+
+})
