03ca4bbac718d6678d6a92d83eca4a70aa72aae6
bkollmar <kollmarb@dickinson.edu>
2023-10-30 16:11:23 -0400

    added a test to check the deafult text for generate report button as well as a test to check the default value of the start date

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -9,4 +9,18 @@ describe("Test the harvest report default values", () => {
     it("Checks for 'Set Dates' section ", () => {
         cy.get("[class=panel-heading]").should("have.text","Set Dates")
     })
+    it("Checks the button has the label Generate Report ", () => {
+        cy.get("[data-cy=generate-rpt-btn]").should("have.text","Generate Report")
+    })
+    it("Checks that the Start Date is the first day of the year ", () => {
+        cy.get('[data-cy=date-select]')
+        .each(($el, index, $all) => {
+            if (index == 0) {
+                expect($el).to.have.value('2023-01-01')
+            }
+        })
+
+    })
+
+
 })
