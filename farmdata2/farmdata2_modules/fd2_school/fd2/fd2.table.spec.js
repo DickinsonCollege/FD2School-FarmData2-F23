57d370512ae91553533ba8cb79e5625c2583bab6
hutterfly <huttere@dickinsno.edu>
2023-10-29 23:42:31 -0400

    added test to test crop filtering

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,7 @@ describe("Testing with the CustomTableComponent", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
       
-    it("do something", () => {
+    it("testing headers", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=h0]").should("have.text","Row")
         cy.get("[data-cy=h1]").should("have.text","Date")
@@ -13,5 +13,16 @@ describe("Testing with the CustomTableComponent", () => {
         cy.get("[data-cy=h4]").should("have.text","Yield")
         cy.get("[data-cy=h5]").should("have.text","Units")
         cy.get("[data-cy=table-headers]").children().should("have.length", 6)
+    })
+
+    it("testing filtering by crop", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").select("ASPARAGUS")
+        cy.get("[data-cy=table-body]").children().should("have.length", "5")
+        cy.get("[data-cy=td-r0c3]").should("contain.text", "ASPARAGUS")
+        cy.get("[data-cy=td-r1c3]").should("contain.text", "ASPARAGUS")
+        cy.get("[data-cy=td-r2c3]").should("contain.text", "ASPARAGUS")
+        cy.get("[data-cy=td-r3c3]").should("contain.text", "ASPARAGUS")
+        cy.get("[data-cy=td-r4c3]").should("contain.text", "ASPARAGUS")
     })
 })
