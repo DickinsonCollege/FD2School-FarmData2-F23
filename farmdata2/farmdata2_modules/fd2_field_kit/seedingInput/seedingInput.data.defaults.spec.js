48b549884e5ac58de70597fa5c0adba236f0c615
charliemax5 <maxwellc@dickinson.edu>
2023-11-01 18:15:01 -0400

    Added  Cypress Tests to seedingInput.data.defaults.spec.js to check if the crop drop down does not have a selected value and that the crop drop down contains the correct crop list

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -1,5 +1,34 @@
 //test the default contents of the "Data" section of the Seeding Input Form in the FieldKit
 //including header, date input element, and crop dropdown, 
 describe('test the Seeding Input Form Data defaults', () => {
+  beforeEach("Log into FarmData and visit FieldKit tab and Seeding Input sub-tab", () => {
+    cy.login('manager1', 'farmdata2')
+    cy.visit('/farm/fd2-field-kit/seedingInput')
+  })
+
+  //Ally's Tests
+  it("", () => {
+
+  })
+
+  //Charlie's Tests
+  it("Checks if crop drop down is correct", () => {
+    //Checks if the drop down has a selected value; it should not
+    cy.get("[data-cy=crop-selection]")
+      .should("have.value","")
+
+    //Checks to see if the drop down contains the correct crop list by checking the length and if certain indexes have the correct values
+    cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").children()
+      .should("have.length","111")
+    cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option0]")
+      .should("have.text","ARUGULA")
+    cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option110]")
+      .should("have.text","ZUCCHINI")
+  })
+
+  //Trang's Tests
+  it("", () => {
+
+  })
 
   })
