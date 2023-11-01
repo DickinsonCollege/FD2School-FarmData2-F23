47e6292d87161bb73bdc0d555db501bd137f3e9c
allisons9988 <smithaj@dickinson.edu>
2023-11-01 14:05:58 -0400

    Added test for seeding input data defaults

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -0,0 +1,5 @@
+//test the default contents of the "Data" section of the Seeding Input Form in the FieldKit
+//including header, date input element, and crop dropdown, 
+describe('test the Seeding Input Form Data defaults', () => {
+
+  })
