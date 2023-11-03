2e6030d541a49ae31ca932ac4d9014503b342ade
Zivanovic8 <zivanova@dickinson.edu>
2023-11-03 12:57:35 -0400

    Added a new It to cypress test that tests if filtering of the table works

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -6,13 +6,21 @@ describe("Test the harvest report default values", () => {
 
     it("Check for table headers", () => {
         cy.get("[data-cy=report-table]").click()
-        cy.get("[data-cy=h0]").should("have.text","Row")
-        cy.get("[data-cy=h1]").should("have.text","Date")
-        cy.get("[data-cy=h2]").should("have.text","Area")
-        cy.get("[data-cy=h3]").should("have.text","Crop")
-        cy.get("[data-cy=h4]").should("have.text","Yield")
-        cy.get("[data-cy=h5]").should("have.text","Units")
-        cy.get("[data-cy=table-headers]").children().should("have.length","6")
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
+        cy.get("[data-cy=table-headers]").children().should("have.length","5")
 
+    })
+
+    it("Check if filtering by crop works", () => {
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").select("HERB-CILANTRO")
+        cy.contains('Generate').click()
+        cy.get("[data-cy=table-body").children().should("have.length","3")
+        cy.get("[data-cy=td-r0c2").contains("HERB-CILANTRO")
+        cy.get("[data-cy=td-r1c2").contains("HERB-CILANTRO")
+        cy.get("[data-cy=td-r2c2").contains("HERB-CILANTRO")
     })
 })
