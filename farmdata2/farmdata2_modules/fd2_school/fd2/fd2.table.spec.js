5ec87c9a12e87b5f377ecf733a72d0f1efede152
Zivanovic8 <zivanova@dickinson.edu>
2023-11-03 12:34:12 -0400

    Added a cypress test for column headers of the tables

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,6 +4,14 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/FD2")
     })
 
-    it("", () => {
+    it("Check for table headers", () => {
+        cy.get("[data-cy=report-table]").click()
+        cy.get("[data-cy=h0]").should("have.text","Row")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
+
     })
 })
