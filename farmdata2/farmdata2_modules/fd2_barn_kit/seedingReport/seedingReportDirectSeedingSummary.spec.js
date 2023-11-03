7f2ef6d51da7393562a0aa09897a64a9ef97e8cb
calebloring <loringc@dickinson.edu>
2023-11-03 14:13:41 -0400

    BeforeEach section completed for seeding report direct summary test

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportDirectSeedingSummary.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportDirectSeedingSummary.spec.js
@@ -2,8 +2,14 @@ const dayjs = require("dayjs")
 
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-barn-kit/seedingReport")
+        cy.get('[data-cy=start-date-select').type('2020-05-05')
+        cy.get('[data-cy=end-date-select').type('2020-05-06')
+        cy.get('[data-cy=generate-rpt-btn').click()
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('BEAN')
     })
-    it("Checks table header", () => {
+    it("", () => {
   
     })
 })
