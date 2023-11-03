84d2b48b3d046d81e63aa90b592465d3ad1b3ad9
allisons9988 <smithaj@dickinson.edu>
2023-11-03 14:16:37 -0400

    Added beforeEach

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.DirectSeeding.SummaryTable.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.DirectSeeding.SummaryTable.spec.js
@@ -1,7 +1,9 @@
 //generates seeding reports to check that the Direct Seeding Summary Table at the bottom of the report
 describe('Test the Direct Seeding Summary Report Table', () => {
-  beforeEach('', () => {
-
+  beforeEach('Log into FarmData and visit BarnKit tab and Seeding Report sub-tab', () => {
+    cy.login('manager1', 'farmdata2')
+    cy.visit('/farm/fd2-barn-kit/seedingReport')
+    cy.waitForPage()
   })
 
 
