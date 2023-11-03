dfaf59e81fa098fac32e75918da93c11c33bc0ea
allisons9988 <smithaj@dickinson.edu>
2023-11-03 14:07:02 -0400

    Created empty seedingReport.DirectSeeding.SummaryTable.spec.js file

---
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.DirectSeeding.SummaryTable.spec.js
@@ -0,0 +1,9 @@
+//generates seeding reports to check that the Direct Seeding Summary Table at the bottom of the report
+describe('Test the Direct Seeding Summary Report Table', () => {
+  beforeEach('', () => {
+
+  })
+
+
+
+  })
