cf1c5ffdf94eac422d2e110e2aac25571fa70440
anemervontage <anemervontage@gmail.com>
2023-11-08 14:42:17 -0500

    added tests for table headers

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -1,9 +1,22 @@
-describe("Test the harvest report default values", () => {
+describe("Test the harvest report table values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/FD2")
+        cy.visit("/farm/fd2-school/fd2")
     })
 
-    it("", () => {
+    it("check table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+
+        cy.get("[data-cy=h0]").should("have.text","Row")
+
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        
+        cy.get("[data-cy=h2]").should("have.text","Area")
+
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+
+        cy.get("[data-cy=h5]").should("have.text","Units")
     })
 })
