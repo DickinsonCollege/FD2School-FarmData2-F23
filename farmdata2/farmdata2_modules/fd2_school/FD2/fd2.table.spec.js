653c90450565a55c6b877a7a42899d226b0a08cc
anemervontage <anemervontage@gmail.com>
2023-11-08 17:17:15 -0500

    added tests for filtering crops by name

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -21,4 +21,20 @@ describe("Test the harvest report table values", () => {
 
         cy.get("[data-cy=table-headers]").children().should("have.length","6")
     })
+
+    it("Check filtering by crop", () => {
+        cy.get("[data-cy=crop-drop] > [data-cy=dropdown-input]").select("ARUGULA")
+
+        cy.get("[data-cy=generate-report-button]").click()
+
+        cy.get("[data-cy=table-body").children().should("have.length","4")
+
+        cy.get("[data-cy=td-r0c3").contains("ARUGULA")
+
+        cy.get("[data-cy=td-r1c3").contains("ARUGULA")
+
+        cy.get("[data-cy=td-r2c3").contains("ARUGULA")
+
+        cy.get("[data-cy=td-r2c3").contains("ARUGULA")
+    })
 })
