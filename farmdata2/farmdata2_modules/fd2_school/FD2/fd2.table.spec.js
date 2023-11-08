bca2e21d5981dec7cf9eb5bc5efa0404c2a60d61
anemervontage <anemervontage@gmail.com>
2023-11-08 14:44:08 -0500

    added tests for number of columns in table

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -18,5 +18,7 @@ describe("Test the harvest report table values", () => {
         cy.get("[data-cy=h4]").should("have.text","Yield")
 
         cy.get("[data-cy=h5]").should("have.text","Units")
+
+        cy.get("[data-cy=table-headers]").children().should("have.length","6")
     })
 })
