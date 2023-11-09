b46ca7b8cea5dea31caa244832d2c56cd687433c
allisons9988 <smithaj@dickinson.edu>
2023-11-09 10:03:31 -0500

    Added test to check that summary table exists and is visible when 'All' or 'Direct Seeding' is selected

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.DirectSeeding.SummaryTable.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.DirectSeeding.SummaryTable.spec.js
@@ -7,6 +7,33 @@ describe('Test the Direct Seeding Summary Report Table', () => {
   })
 
   //Ally's Test
+  it("Checks that the table exists and is visible", () => {
+
+    //select dates
+    cy.get("[data-cy=date-range-selection]")
+    cy.get("[data-cy=date-range-selection] [data-cy=date-select]").eq(0)
+      .click()  
+      .type("2020-01-01")
+    cy.get("[data-cy=date-range-selection] [data-cy=date-select]").eq(1)
+      .click()  
+      .type("2023-01-01")
+
+    //click generate report button
+    cy.get("[data-cy=generate-rpt-btn]").click()
+
+    //check when all is selected
+    cy.get("[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]").select(0)
+    cy.get("[data-cy=report-table]")
+      .should("exist")
+      .should("be.visible")
+
+    //check when direct seeding is selected
+    cy.get("[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]").select(1)
+    cy.get("[data-cy=report-table]")
+      .should("exist")
+      .should("be.visible")
+
+  })
 
   //Trang's Test
 
