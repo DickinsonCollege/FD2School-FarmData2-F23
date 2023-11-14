912df66f886121e5d83f32a06b52ccfd624b7f34
calebloring <112512660+calebloring@users.noreply.github.com>
2023-11-14 10:20:06 -0500

    Added test to ensure that the crop list is the correct length

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportCropList.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportCropList.spec.js
@@ -4,11 +4,14 @@ describe("Test the seeding report summary values values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-barn-kit/seedingReport")
-        cy.get('[data-cy=start-date-select').type('2020-05-05')
-        cy.get('[data-cy=end-date-select').type('2020-05-06')
-        cy.get('[data-cy=r0-edit-button').click()
-        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('BEAN')
+        cy.get('[data-cy=start-date-select]').type('2020-05-05')
+        cy.get('[data-cy=end-date-select]').type('2020-05-06')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy=r0-edit-button]').click()
+        
     })
-    
+    it("Tests crop list length", () => {
+        cy.get('[data-cy=r0-Crop-input]').children().should('have.length',111)
+    })
     
 })
