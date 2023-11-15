a97d9d4b8eb709fad4c333fd8d0ebcc441f35f75
allisons9988 <smithaj@dickinson.edu>
2023-11-15 10:16:02 -0500

    Created empty test file

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.database.newLog.spec.js
@@ -0,0 +1,9 @@
+//Checks that a new tray seeding log is added to the database correctly after "Submit" button is clicked
+describe('Check that tray seeding log is added to database', () => {
+    beforeEach('', () => {
+  
+    })
+  
+  
+  
+    })
