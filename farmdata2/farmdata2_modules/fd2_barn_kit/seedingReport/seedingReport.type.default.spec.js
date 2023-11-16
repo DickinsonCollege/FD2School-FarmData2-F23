f3f10f1b724eaaaa4a7117a9ae6f46415452eb28
aidanbennettjones <bennetai@dickinson.edu>
2023-11-16 12:21:00 -0500

    Added Test #3 for Issue22

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.type.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.type.default.spec.js
@@ -19,4 +19,23 @@ describe("Testing for Area Filter", () => {
         cy.get('[data-cy=td-r11c2]').contains('SEEDING BENCH')
         cy.get('[data-cy=td-r66c2]').contains('ALF-3')
     })
+
+    //Test #3
+    it("Check that area filter only contains areas where there are seeding logs", () => {
+        cy.get('[data-cy=start-date-select] > [data-cy=date-select]').type('2020-05-05').blur();
+        cy.get('[data-cy=end-date-select] > [data-cy=date-select]').type('2020-05-15').blur();
+    
+        cy.get('[data-cy=generate-rpt-btn]').click();
+    
+        cy.get('[data-cy=area-dropdown] > [data-cy=dropdown-input]')
+            .children()
+            .should('have.length.gte', 1)
+            .then(($children) => {
+                // Asserting that at least one child element exists
+                expect($children).to.have.length.gte(1);
+    
+                // Checking if the child elements contain specific values
+                cy.wrap($children.eq(0)).should('have.value', 'All');
+            });
+    });
 })
