a3597b22d97577df3bad0a9436c4bb4df90b50a6
HaNguyen <nguyenhd@dickinson.edu>
2023-11-30 22:33:57 -0500

    create test for the report data

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.button.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.button.spec.js
@@ -13,4 +13,11 @@ describe("Test the harvest report using the generate button", () => {
         cy.get("[data-cy=details]").should("be.visible")
         cy.get("[data-cy=harvest-report-table]").should("be.visible")
     })
+
+    it("Check the data in the report form", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text", "Farm:Sample Farm")
+        cy.get("[data-cy=username]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "English")
+    })
 })
