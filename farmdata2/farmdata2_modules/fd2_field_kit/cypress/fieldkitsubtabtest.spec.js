425105bc422e560f7f3b601107a5c818dfc69f03
anemervontage <anemervontage@gmail.com>
2023-12-01 14:18:37 -0500

    added spec file for Subtab testing

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldkitsubtabtest.spec.js
@@ -0,0 +1,21 @@
+describe("Testing for Field-kit Sub-tabs", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-field-kit")
+    })
+
+    //Test 1
+    it("Check that the FieldKit tab contains sub-tabs for “Info” and “Seeding Input", () => {
+    
+    })
+
+    //Test 2
+    it("Check that the order of the tabs is “Info” and then “Seeding Input", () => {
+    
+    })
+
+    //Test 3
+    it("Check that there are the correct number of sub-tabs (2 at this time)", () => {
+    
+    })
+})
