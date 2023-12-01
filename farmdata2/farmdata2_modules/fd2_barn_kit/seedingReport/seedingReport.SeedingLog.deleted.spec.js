291c86f2bd3fa4643a93f31afcaedaf528e66885
nghtlinh <nghtlinh25@gmail.com>
2023-11-30 21:31:51 -0500

    Added beforeEach block

---
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.SeedingLog.deleted.spec.js
@@ -0,0 +1,9 @@
+describe('Test the seeding input page', () => {
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-barn-kit/seedingReport')
+        cy.waitForPage()
+    }) 
+    
+
+})
