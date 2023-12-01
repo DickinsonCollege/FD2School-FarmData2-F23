636a456ea696f5006ebf54b9561a46bf37dddfad
balemae <balemae@dickinson.edu>
2023-12-01 13:46:03 -0500

    initial tests for delete-action cancel

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.SeedingLog.deleted.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.SeedingLog.deleted.spec.js
@@ -31,4 +31,37 @@ describe('Test the seeding input page', () => {
         });
     })
 
+    it('cancel deleting row and checking that it was not removed from table and database', () => {
+        cy.get('[data-cy=start-date-select]')
+                .type('2019-01-01')
+        cy.get('[data-cy=end-date-select]')
+                .type('2019-02-15')
+        cy.get('[data-cy=generate-rpt-btn]')
+                .click()
+        //cy.get('[data-cy="report-table"] tbody tr').its('length').as('initialRowCount');
+
+         // Check the checkbox of the first row
+        cy.get('[data-cy="report-table"] tbody tr:first-child input[type="checkbox"]').check();
+
+        cy.viewport(1500, 1500) 
+         // Click the "Delete" and cancel button
+         cy.get('[data-cy=delete-button]')
+            .should('exist')
+                 .click({force:true})
+         cy.get('[data-cy=cancel-button]')
+                 .should('exist')
+                 .click()
+ 
+         // Wait for the deletion process to complete
+         cy.waitForPage()
+ 
+         // Get the initialRowCount from the alias
+         cy.get('@initialRowCount').then((initialRowCount) => {
+             cy.get('[data-cy="report-table"] tbody tr').should('have.length', initialRowCount - 1);
+         });
+
+
+
+    })
+
 })
