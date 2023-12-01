75f9caa577052d087bb4174654276bbc3820b7f3
nghtlinh <nghtlinh25@gmail.com>
2023-12-01 14:49:48 -0500

    Draft tests for multiple deletions and cancel deletion

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.SeedingLog.deleted.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.SeedingLog.deleted.spec.js
@@ -9,7 +9,7 @@ describe('Test the seeding input page', () => {
         cy.get('[data-cy=start-date-select]')
                 .type('2019-01-01')
         cy.get('[data-cy=end-date-select]')
-                .type('2019-02-15')
+                .type('2019-03-15')
         cy.get('[data-cy=generate-rpt-btn]')
                 .click()
         cy.get('[data-cy="report-table"] tbody tr').its('length').as('initialRowCount');
@@ -18,9 +18,37 @@ describe('Test the seeding input page', () => {
         cy.get('[data-cy="report-table"] tbody tr:first-child input[type="checkbox"]').check();
         
 
-        // Click the "Delete" button
+        // Click the "Delete" buttonss
         cy.get('[data-cy=delete-button]')
                 .click()
+
+        // Wait for the deletion process to complete
+        cy.waitForPage()
+
+        // Get the initialRowCount from the alias
+        cy.get('@initialRowCount').then((initialRowCount) => {
+            cy.get('[data-cy="report-table"] tbody tr').should('have.length', initialRowCount - 1);
+        });
+    })
+
+    it('should delete many rows and verify removal from the table and database', () => {
+        cy.get('[data-cy=start-date-select]')
+                .type('2019-01-01')
+        cy.get('[data-cy=end-date-select]')
+                .type('2019-03-15')
+        cy.get('[data-cy=generate-rpt-btn]')
+                .click()
+        cy.get('[data-cy="report-table"] tbody tr').its('length').as('initialRowCount');
+
+        // Check the checkbox of the first row
+        cy.get('[data-cy="report-table"] tbody tr:first-child input[type="checkbox"]').check();
+        cy.get('[data-cy="report-table"] tbody tr:first-child input[type="checkbox"]').check();
+        
+        // Click the "Delete" buttonss
+        cy.get('[data-cy=delete-button]') .click()
+        cy.on("window:confirm", function(alert){
+                return true;})
+        //cy.contains("OK").click();
 
         // Wait for the deletion process to complete
         cy.waitForPage()
@@ -35,7 +63,7 @@ describe('Test the seeding input page', () => {
         cy.get('[data-cy=start-date-select]')
                 .type('2019-01-01')
         cy.get('[data-cy=end-date-select]')
-                .type('2019-02-15')
+                .type('2019-03-15')
         cy.get('[data-cy=generate-rpt-btn]')
                 .click()
         //cy.get('[data-cy="report-table"] tbody tr').its('length').as('initialRowCount');
@@ -43,14 +71,12 @@ describe('Test the seeding input page', () => {
          // Check the checkbox of the first row
         cy.get('[data-cy="report-table"] tbody tr:first-child input[type="checkbox"]').check();
 
-        cy.viewport(1500, 1500) 
+        // cy.viewport(1500, 1500) 
          // Click the "Delete" and cancel button
          cy.get('[data-cy=delete-button]')
-            .should('exist')
-                 .click({force:true})
-         cy.get('[data-cy=cancel-button]')
-                 .should('exist')
                  .click()
+        cy.on("window:confirm", function(alert){
+                return false;})
  
          // Wait for the deletion process to complete
          cy.waitForPage()
